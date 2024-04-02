2b549117ab216551c18cd006072fdb03c0db8ab2
RubezhkinLabs <pavel-ned-03@mail.ru>
2024-04-02 10:56:01 +0300

    modified:   demo/src/main/java/com/example/Main.java 	new file:   demo/target/classes/com/example/Main.class

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,7 +1,49 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        test(1000);
+    }
+    public static void test(int count){
+        ArrayList<Integer> aList = new ArrayList<>();
+        long startTime = System.nanoTime();
+        for(int i = 0; i < count; i++)
+            aList.add(i);
+        long endTime = System.nanoTime();
+        System.out.println("ArrayList add " + count + " " + (endTime-startTime)/1000000.0);
+
+        startTime = System.nanoTime();
+        for(int i = 0; i < count; i++)
+            aList.get(i);
+        endTime = System.nanoTime();
+        System.out.println("ArrayList get " + count + " " + (endTime-startTime)/1000000.0);
+
+        startTime = System.nanoTime();
+        for(int i = count-1; i >= 0; i--)
+            aList.remove(i);
+        endTime = System.nanoTime();
+        System.out.println("ArrayList remove " + count + " " + (endTime-startTime)/1000000.0);
+
+        LinkedList<Integer> lList = new LinkedList<>();
+        startTime = System.nanoTime();
+        for(int i = 0; i < count; i++)
+            lList.add(i);
+        endTime = System.nanoTime();
+        System.out.println("LinkedList add " + count + " " + (endTime-startTime)/1000000.0);
+
+        startTime = System.nanoTime();
+        for(int i = 0; i < count; i++)
+            lList.get(i);
+        endTime = System.nanoTime();
+        System.out.println("LinkedList get " + count + " " + (endTime-startTime)/1000000.0);
+
+        startTime = System.nanoTime();
+        for(int i = count-1; i >= 0; i--)
+            lList.remove(i);
+        endTime = System.nanoTime();
+        System.out.println("LinkedList remove " + count + " " + (endTime-startTime)/1000000.0);
     }
 }
